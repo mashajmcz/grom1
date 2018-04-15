dc4744fb626af22c37e518dac9c3ce9f4b5c0c92
mashajmcz <38409688+mashajmcz@users.noreply.github.com>
2018-04-16 01:49:57 +0300

    Update and rename secondaryAccount.java to SecondaryAccount.java

---
+++ accounts/SecondaryAccount.java
@@ -0,0 +1,7 @@
+package lesson8.accounts;
+
+public class SecondaryAccount extends CheckingAccount {
+    public SecondaryAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpences) {
+        super(bankName, ownerName, moneyAmount, limitOfExpences);
+    }
+}
--- accounts/secondaryAccount.java
+++
@@ -1,7 +0,0 @@
-package lesson8.accounts;
-
-public class secondaryAccount extends CheckingAccount {
-    public secondaryAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpences) {
-        super(bankName, ownerName, moneyAmount, limitOfExpences);
-    }
-}
