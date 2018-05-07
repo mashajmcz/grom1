318218cac882e48c5c7199f3866972aea2e8e75f
MashaJM <masha.chip@gagadget.com>
2018-05-07 22:49:09 +0300

    w3

--- hw8_2/Demo.java
+++ hw8_2/Demo.java
@@ -3,19 +3,24 @@ package lesson8.hw8_2;
 import java.util.Date;
 
 public class Demo {
-        public Student createHighestsParent(){
-            Student student1 = new Student("Johnny", "Walker",01,null);
-            Student student2 = new Student("Ivy", "Briggs",01,null);
-            Student[] group01 = {student1, student2};
-            Date date = new Date();
-            Course course = new Course(date,"Functional Analysis", 50,
-                    "mr. Bernstein",group01);
-            return student1;
-        }
-        public Student createLowestChild(){
-            
+    public Student createHighestParent() {
+        Student student1 = new Student("Johnny", "Walker", 01, null);
+        Student student2 = new Student("Ivy", "Briggs", 01, null);
+        Student[] group01 = {student1, student2};
+        Date date = new Date();
+        Course course = new Course(date, "Functional Analysis", 50,
+                "mr. Bernstein", group01);
+        return student1;
+    }
 
-            return new SpecialStudent("Andrew", "Smith", 01, null, 123,
-                    "andrew@gmail.com");
+    public Student createLowestChild() {
+
+        Student specialStudent = new SpecialStudent("Andrew", "Smith", 02, null, 123,
+                "andrew@gmail.com");
+        Date date = new Date();
+        Student[] group02 = {specialStudent};
+        Course course = new Course(date, "Functional Analysis", 50, "mr. Bernstein", group02);
+        return specialStudent;
+
     }
 }
